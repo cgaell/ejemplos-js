f07390cb2e60c589170c71e0b610f59f0948fd51
Gael Castro <gaelcastrolara2006@gmail.com>
2026-01-21 14:26:52 -0600

    a

--- calculadora.js
+++ calculadora.js
@@ -1,16 +1,35 @@
 // comentario comentado
 alert("Adios mundo cruel")
-function mostrar() {
+function suma(e) {
     let numero1 = parseInt(document.getElementById("numero1").value);
     let numero2 = parseInt(document.getElementById("numero2").value);
     
     let resultado = 0;
+    let operacion = e.target.id;
 
-    resultado = (numero1 + numero2);
+    if (operacion == "suma"){
+        resultado = (numero1 + numero2);
+    }
+    else if (operacion == "resta"){
+        resultado = numero1 - numero2;
+    }
+    else if (operacion == "multiplica"){
+        resultado = numero1 * numero2;
+    }
+    else if (operacion == "divide"){
+        resultado = numero1 / numero2;
+    }
+    else{
+        alert("Funcionalidad en progreso");
+    }
+    
+    
     
 
-    let objResultado = document.getElementById("resultado");
-    objResultado.innerHTML = resultado;
+    let result = document.getElementById("resultado");
+   result.innerHTML = resultado;
 
 
 }
+let botones = document.getElementById("btns");
+botones.addEventListener("click", suma)
