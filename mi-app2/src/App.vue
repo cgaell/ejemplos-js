4bf3d4678ad1bf362af1b8dec5e51750f193f4cf
Gael Castro <gaelcastrolara2006@gmail.com>
2026-01-26 15:26:09 -0600

    .

---
+++ mi-app2/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="app">
+<h1>Hola Mundo desde el curso de Desarrollo
+Fullstack</h1>
+</div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+#app {
+font-family: Avenir, Helvetica, Arial, sans-serif; text-align: center; color: #2c3e50;
+margin-top: 60px; }
+</style>
